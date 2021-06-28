cc16bf6436ec098c441272cd50d0c8c3d9dedbb7
hermitli <hermitli@tencent.com>
2021-06-28 17:31:40 +0800

    ASE WIP

--- sample_elimination.py
+++ sample_elimination.py
@@ -30,7 +30,6 @@ domainArea = 1.0 * 1.0 / outputNum
 domainDimension = 2
 
 rMax = math.sqrt(domainArea / (2 * math.sqrt(3)))
-rMin = utils.getWeightLimitFraction(inputNum, outputNum)
 for i in range(inputNum):
     neighbourIndices = tree.query_ball_point(initialSamples[i], r = 2.0 * rMax)
     for neighbourIndex in neighbourIndices:
@@ -93,5 +92,79 @@ for iterationIndex in range(IterationNum):
                 deltaX += movement
         sample += deltaX
         outputSamples[i] = sample
+utils.displayVecArray(outputSamples)
+
+# %%
+import numpy as np
+import utils
+from max_heap import MaxHeap
+from scipy import spatial
+from sklearn.metrics.pairwise import pairwise_distances
+import math
+
+np.random.seed(0)
+
+alpha = 8
+
+inputNum = 250
+outputNum = 70
+nearestNeighborCount = 8
+initialSamples = np.random.random((inputNum, 2))  # 5 points in 2 dimensions
+
+utils.displayVecArray(initialSamples)
+
+tree = spatial.KDTree(initialSamples)
+# Compute RMax List
+rMaxs = np.zeros(inputNum)
+sr = np.zeros(inputNum)
+ratio = float(inputNum) / float(outputNum)
+for i in range(len(initialSamples)):
+    initialSample = initialSamples[i]
+    dd, ii = tree.query(initialSample, nearestNeighborCount)
+    radius = dd[len(dd) - 1]
+    sampleArea = ratio * (math.pi * radius * radius) / float(len(dd))
+    rMaxs[i] = math.sqrt(sampleArea / (2.0 * math.sqrt(3.0)))
+    sr[i] = 2.0 * rMaxs[i]
+
+weights = np.zeros(inputNum)
+
+for index in range(inputNum):
+    dMax = 2.0 * rMaxs[index]
+    neighbourIndices = tree.query_ball_point(initialSamples[index], r = dMax)
+    for i in neighbourIndices:
+        if i >= inputNum:
+            continue
+        if i != index:
+            squaredDistance = pairwise_distances([initialSamples[index]], [initialSamples[i]]).flatten()[0]
+            weights[index] += utils.weightFunction(squaredDistance, dMax)
+            # Why
+            if sr[i] < dMax:
+                sr[i] = dMax
+
+heap = MaxHeap()
+heap.SetData(weights)
+heap.Build()
+
+outputSamples = []
+remainSampleNum = inputNum
+while remainSampleNum > outputNum:
+    index = int(heap.GetTopItemID())
+    heap.Pop()
+
+    neighbourIndices = tree.query_ball_point(initialSamples[index], r = sr[index])
+    for i in neighbourIndices:
+        if i >= inputNum:
+            continue
+        if i != index:
+            neighbourDMax = 2.0 * rMaxs[i]
+            squaredDistance = pairwise_distances([initialSamples[index]], [initialSamples[i]]).flatten()[0]
+            if (neighbourDMax * neighbourDMax) > squaredDistance:
+                weights[i] -= utils.weightFunction(squaredDistance, neighbourDMax)
+                heap.MoveItemDown(i)
+    remainSampleNum -= 1
+
+for i in range(outputNum):
+    outputSamples.append(initialSamples[int(heap.GetIDFromHeap(i))])
+
 utils.displayVecArray(outputSamples)
 # %%
